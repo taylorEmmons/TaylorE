749ecc7da1945290e4898c4e1109dfbac8c71ec8
taylorEmmons <taylor.emmons@mtchs.org>
2014-02-14 09:25:42 -0700

    Deleted Stuff add test.txt
    woot

--- Casino/src/casino/House.java
+++
@@ -1,9 +0,0 @@
-package casino;
-
-
-public enum House{
-	HEARTS, SPADES, CLUBS, DIAMONDS;
-	
-	public static final int length = 4;
-	
-}
--- Casino/src/casino/Value.java
+++
@@ -1,9 +0,0 @@
-package casino;
-
-
-public enum Value {
-	ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
-	
-	public final static int length = 13;
-	
-}
